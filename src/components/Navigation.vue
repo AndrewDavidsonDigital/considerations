592ebeb5ccd9873835e65a3e6cbb01f57cf146a8
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-07-03 13:05:30 +1000

    UI: Fix mobile navigation issues

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -21,7 +21,7 @@ watch(isMenuOpen, (newVal) => {
 </script>
 
 <template>
-  <nav class="relative max-h-5 !mt-0 mb-2">
+  <nav class="md:relative max-h-5 !mt-0 mb-2">
     <button 
       class="ml-auto mr-2 cursor-pointer w-fit flex gap-2 group"
       @click="() => (isMenuOpen = !isMenuOpen)"
@@ -33,34 +33,50 @@ watch(isMenuOpen, (newVal) => {
     </button>
 
     <aside 
+      class="
+        bg-slate-500 md:bg-gradient-to-r from-slate-500 to-[--color-background] to-95
+        absolute right-0 top-0 md:top-12
+        text-xl md:text-base
+        w-0 md:max-w-[400px] max-w-screen
+        md:h-auto
+        glass z-menu ml-auto rounded-l-xl
+        transition-all duration-500
+        pl-5 pr-2
+      "
       :class="[
-        'bg-slate-500 md:bg-gradient-to-r from-slate-500 to-[--color-background] to-95',
-        'absolute right-0 top-12',
-        'text-xl md:text-base',
-        'w-0 max-w-[400px]', 
-        'glass z-menu ml-auto rounded-l-xl',
-        'transition-all duration-500',
-        'px-5',
-        { 'w-full h-[80dvh] md:!h-auto md:!w-1/4' : isMenuOpen },
+        { 'w-full h-[100dvh] md:!h-auto md:!w-1/4' : isMenuOpen },
         { 'translate-x-8 opacity-0 !px-0' : !isMenuOpen },
       ]"
     >
-      <section class="py-10 flex flex-col gap-y-2 overflow-hidden">
+      <section class="md:py-10 overflow-y-scroll md:overflow-y-clip overflow-x-clip max-h-[100dvh] ">
+        <div class="my-2 md:hidden">
+          <button 
+            class="mx-auto cursor-pointer w-fit flex gap-2 group"
+            @click="() => (isMenuOpen = !isMenuOpen)"
+          >
+            <h2 class="text-xl duration-500 text-emerald-400 underline underline-offset-2">
+              Collapse
+            </h2>
+          </button>
+        </div>
         <template
           v-for="(value, _index) in routes"
           :key="'routerKey_' + _index"
         >
-          <RouterLink
-            :to="value.path"
-            class="truncate duration-500 hover:text-orange-400"
-            :class="[
-              { 'pointer-events-none cursor-default truncate !text-cyan-500' : value.path === currentRoute.path},
-            ]"
-            :title="value.title"
-            @click="isMenuOpen = false"
-          >
-            {{ value.title }}
-          </RouterLink>
+          <div class="mb-2">
+            <RouterLink
+              v-if="value.path !== '/'"
+              :to="value.path"
+              class="truncate duration-500 hover:text-orange-400"
+              :class="[
+                { 'pointer-events-none cursor-default truncate !text-cyan-500' : value.path === currentRoute.path},
+              ]"
+              :title="value.title"
+              @click="isMenuOpen = false"
+            >
+              {{ value.title }}
+            </RouterLink>
+          </div>
         </template>
       </section>
     </aside> 
